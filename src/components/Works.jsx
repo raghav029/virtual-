9a233397b197d55111c293cc0ed57e0fb0d1ab22
Raghav Kumar jha <jraghavkumar029@gmail.com>
2023-12-03 18:03:29 +0530

    second

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,84 +1,84 @@
-import React from "react";
-import Tilt from "react-tilt";
-import { motion } from "framer-motion";
+// import React from "react";
+// import Tilt from "react-tilt";
+// import { motion } from "framer-motion";
 
-import { styles } from "../styles";
-import { github } from "../assets";
-import { SectionWrapper } from "../hoc";
-import { projects } from "../constants";
-import { fadeIn, textVariant } from "../utils/motion";
+// import { styles } from "../styles";
+// import { github } from "../assets";
+// import { SectionWrapper } from "../hoc";
+// import { projects } from "../constants";
+// import { fadeIn, textVariant } from "../utils/motion";
 
-const ProjectCard = ({
-  index,
-  name,
-  description,
-  tags,
-  image,
-  source_code_link,
-}) => {
-  return (
-    <motion.div variants={fadeIn("up", "spring", index * 0.5, 0.75)}>
-      <Tilt
-        options={{
-          max: 45,
-          scale: 1,
-          speed: 450,
-        }}
-        className='bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full'
-      >
-        <div className='relative w-full h-[230px]'>
-          <img
-            src={image}
-            alt='project_image'
-            className='w-full h-full object-cover rounded-2xl'
-          />
-        </div>
+// const ProjectCard = ({
+//   index,
+//   name,
+//   description,
+//   tags,
+//   image,
+//   source_code_link,
+// }) => {
+//   return (
+//     <motion.div variants={fadeIn("up", "spring", index * 0.5, 0.75)}>
+//       <Tilt
+//         options={{
+//           max: 45,
+//           scale: 1,
+//           speed: 450,
+//         }}
+//         className='bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full'
+//       >
+//         <div className='relative w-full h-[230px]'>
+//           <img
+//             src={image}
+//             alt='project_image'
+//             className='w-full h-full object-cover rounded-2xl'
+//           />
+//         </div>
 
-        <div className='mt-5'>
-          <h3 className='text-white font-bold text-[24px]'>{name}</h3>
-          {/* <p className='mt-2 text-secondary text-[14px]'>{description}</p> */}
-        </div>
+//         <div className='mt-5'>
+//           <h3 className='text-white font-bold text-[24px]'>{name}</h3>
+//           {/* <p className='mt-2 text-secondary text-[14px]'>{description}</p> */}
+//         </div>
 
-        {/* <div className='mt-4 flex flex-wrap gap-2'>
-          {tags.map((tag) => (
-            <p
-              key={`${name}-${tag.name}`}
-              className={`text-[14px] ${tag.color}`}
-            >
-              #{tag.name}
-            </p>
-          ))}
-        </div> */}
-      </Tilt>
-    </motion.div>
-  );
-};
+//         {/* <div className='mt-4 flex flex-wrap gap-2'>
+//           {tags.map((tag) => (
+//             <p
+//               key={`${name}-${tag.name}`}
+//               className={`text-[14px] ${tag.color}`}
+//             >
+//               #{tag.name}
+//             </p>
+//           ))}
+//         </div> */}
+//       </Tilt>
+//     </motion.div>
+//   );
+// };
 
-const Works = () => {
-  return (
-    <>
-      <motion.div variants={textVariant()}>
-        <p className={`${styles.sectionSubText} `}>Section 1</p>
-        <h2 className={`${styles.sectionHeadText}`}>SCI as a pioneering peace organization</h2>
-      </motion.div>
+// const Works = () => {
+//   return (
+//     <>
+//       <motion.div variants={textVariant()}>
+//         <p className={`${styles.sectionSubText} `}>Section 1</p>
+//         <h2 className={`${styles.sectionHeadText}`}>SCI as a pioneering peace organization</h2>
+//       </motion.div>
 
-      <div className='w-full flex'>
-        <motion.p
-          variants={fadeIn("", "", 0.1, 1)}
-          className='mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]  text-justify'
-        >
-          SCI’s activities started in Western Europe: France, Switzerland and Liechtenstein, soon also Great Britain. The volunteers were of European origin. After the Second World War, SCI started to spread across Western Europe, including the formerly fascist countries such as Germany, Austria, and Italy. And soon afterwards, activities, groups and branches were set up in a few Asian countries, from the Indian subcontinent to Japan and Malaysia. Volunteer exchange with partner organisations was also an important pillar of SCI’s geographical expansion – in communist Eastern Europe as well as on the African continent. After the end of the cold war, SCI spread in Eastern Europe, and a few rare new branches were built up in Africa, Latin America and Asia.
+//       <div className='w-full flex'>
+//         <motion.p
+//           variants={fadeIn("", "", 0.1, 1)}
+//           className='mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]  text-justify'
+//         >
+//           SCI’s activities started in Western Europe: France, Switzerland and Liechtenstein, soon also Great Britain. The volunteers were of European origin. After the Second World War, SCI started to spread across Western Europe, including the formerly fascist countries such as Germany, Austria, and Italy. And soon afterwards, activities, groups and branches were set up in a few Asian countries, from the Indian subcontinent to Japan and Malaysia. Volunteer exchange with partner organisations was also an important pillar of SCI’s geographical expansion – in communist Eastern Europe as well as on the African continent. After the end of the cold war, SCI spread in Eastern Europe, and a few rare new branches were built up in Africa, Latin America and Asia.
 
-        </motion.p>
-      </div>
+//         </motion.p>
+//       </div>
 
-      <div className='mt-20 flex flex-wrap gap-7'>
-        {projects.map((project, index) => (
-          <ProjectCard key={`project-${index}`} index={index} {...project} />
-        ))}
-      </div>
-    </>
-  );
-};
+//       <div className='mt-20 flex flex-wrap gap-7'>
+//         {projects.map((project, index) => (
+//           <ProjectCard key={`project-${index}`} index={index} {...project} />
+//         ))}
+//       </div>
+//     </>
+//   );
+// };
 
-export default SectionWrapper(Works, "");
+// export default SectionWrapper(Works, "");
